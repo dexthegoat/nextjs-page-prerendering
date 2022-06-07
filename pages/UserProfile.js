7fde8976b3a3eafc478c88b871857f105b841116
Jianshuo Yang <jsy.bornconfused@gmail.com>
2022-06-07 15:39:55 -0400

    .

--- pages/UserProfile.js
+++ pages/UserProfile.js
@@ -1,4 +1,18 @@
+import { useEffect } from 'react';
+import useSWR from 'swr';
+
 export default function UserProfilePage({ username }) {
+  // client-side data fetching
+  const { data, error } = useSWR('some-api');
+
+  useEffect(() => {
+    // some data transformation logic and maybe you can set some state
+  }, [data]);
+
+  if (error) return <p>Failed to load...</p>;
+
+  if (!data) return <p>Loading...</p>;
+
   return <h1>{username}</h1>;
 }
 
